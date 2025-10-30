971d17e488c9c5032205eaa333eff3506d1177b3
NhungHuyen2110 <142028904+NhungHuyen2110@users.noreply.github.com>
2025-10-30 22:08:33 +0700

    Refactor getProductById for simplicity
    Simplified getProductById method by removing error handling and unnecessary checks.

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -110,18 +110,8 @@ class ProductController {
     }
   }
   async getProductById(req, res) {
-    try {
-      const { id } = req.params;  // lấy id từ URL
-      const product = await Product.findById(id);
-
-      if (!product) {
-        return res.status(404).json({ message: "Product not found" });
-      }
-
-      res.json(product);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
+      const product = await Product.findById(req.params.id);
+      res.status(200).json(product);
   }
 }
 
